88368e6cc03bf5502dc97a695148abe2a064b80c
lil-cookie <cookie@ro.ru>
2017-04-12 21:12:00 +0300

    seo

---
+++ insta_001/Controllers/SeoController.cs
@@ -0,0 +1,20 @@
+﻿using insta_001.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace insta_001.Controllers
+{
+    public class SeoController : Controller
+    {
+        // GET: Seo
+        public ActionResult Index()
+        {
+            SeoQuery s = new SeoQuery();
+            s.Main();
+            return View();
+        }
+    }
+}
---
+++ insta_001/Parser/SeoQuery.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace insta_001.Parser
+{
+    public class SeoQuery: Parser
+    {
+        private List<string> redirectUrls = new List<string>();
+
+        public void Main()
+        {
+
+            var path = System.Web.Hosting.HostingEnvironment.MapPath(@"~/Parser/urls.txt");
+            redirectUrls = File.ReadLines(path).ToList();
+
+            foreach (var url in redirectUrls)
+            {
+                //ReadHtmlFile(url, Encoding.UTF8);
+                System.Diagnostics.Process.Start(url);
+                // WebRequest wrGETURL = WebRequest.Create(url);
+                // wrGETURL.GetResponse();
+            }
+
+        }
+    }
+}
