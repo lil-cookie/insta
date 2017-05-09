f81b4dd6a1caf03b758b2e39c9433955ea15afb4
lil-cookie <cookie@ro.ru>
2017-05-09 16:31:12 +0300

    sort with bugs

--- insta_001/Controllers/InstController.cs
+++ insta_001/Controllers/InstController.cs
@@ -15,6 +15,7 @@ namespace insta_001.Controllers
         {
             InstParser p = new InstParser();
             List<InstModel> comms = new List<InstModel>();
+
             if (isMyAcc == true)
             {
                 comms = p.Main(true);
@@ -26,27 +27,54 @@ namespace insta_001.Controllers
 
             if (comms != null)
             {
-                comms = comms.Where(ev => ev.comments.Count > 0).OrderByDescending(ev => ev.info.created).ToList();
-                // IEnumerable<Data> data = comms.Where(ev => ev.comments.Count > 0).OrderByDescending(ev => ev.info.createdAt);//.OrderBy(ev => ev.postAuthor)
-                //  comms.Remove(comms.Where(ev => ev.comments.Count == 0));// = (List<Data>)comms.Where);
-
-                for (int i = 0; i < comms.Count(); i++)
-                {
-                    for (int j = 0; j < comms.Count() - i - 1; j++)
-                    {
-                        if (comms[j].comments.Last().created < comms[j + 1].comments.Last().created)
-                        {
-                            var temp = comms[j];
-                            comms[j] = comms[j + 1];
-                            comms[j + 1] = temp;
-                        }
-                    }
-                }
-                return View(comms);
+                comms = SortByCommCreated(comms);
+                return PartialView("Index", comms);
             }
-            else return View(new List<InstModel>());
+
+            else return PartialView(new List<InstModel>());
+        }
+
+        [HttpPost]
+        public ActionResult AjaxSortByPostCreated(List<InstModel> comms)
+        {
+            var val = Request.Form;
+            var val2 = Request.Url;
+
+
+            if (comms != null)
+            {
+                comms = comms.Where(ev => ev.comments.Count > 0).OrderByDescending(ev => ev.info.created).ToList();
+            }
+            return PartialView("AjaxUpdate", comms);
+        }
+
+        [HttpPost]
+        public ActionResult AjaxSortByCommCreated(List<InstModel> comms)
+        {
+            var val = Request.Form;
+            var val2 = Request.Url;
+
+            comms = SortByCommCreated(comms);
+            return PartialView("AjaxUpdate", comms);
         }
 
 
+        public List<InstModel> SortByCommCreated(List<InstModel> comms)
+        {
+            //comms = comms.Where(ev => ev.comments.Count > 0).ToList();
+            for (int i = 0; i < comms.Count(); i++)
+            {
+                for (int j = 0; j < comms.Count() - i - 1; j++)
+                {
+                    if (comms[j].comments.Last().created < comms[j + 1].comments.Last().created)
+                    {
+                        var temp = comms[j];
+                        comms[j] = comms[j + 1];
+                        comms[j + 1] = temp;
+                    }
+                }
+            }
+            return comms;
+        }
     }
 }
